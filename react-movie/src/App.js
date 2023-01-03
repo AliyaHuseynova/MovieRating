e6ae01437dedcc150a32bb935aa80ec58b711dca
AliyaHuseynova <113546368+AliyaHuseynova@users.noreply.github.com>
2023-01-04 01:11:24 +0400

    .

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+// import './App.css';
+// import React from 'react';
 
-const App = () => {
-  return <div>movie app</div>;
-};
+// const App = () => {
+//   return <div>movie app</div>;
+// };
 
 // function App() {
   // return (
@@ -29,4 +29,55 @@ const App = () => {
 //   );
 // }
 
+import React, { useEffect, useState} from 'react';
+
+function App() {
+  const [Movies, fetchMovies] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/movies')
+    .then((resultat) => resultat.json())
+    .then((resultat) => {
+      fetchMovies(resultat)
+      console.log(resultat);
+    })
+  }, []);
+  return (
+  <div className='divclass'>
+    <table className='table'>
+      <tr>
+        <th>Name</th>
+        <th>Year</th>
+        <th>Genre</th>
+        <th>Rating</th>
+      </tr>
+      {Movies.map ((item, i) => {
+        return <tr>
+          <td>{item.Series_Title}</td>
+          <td>{item.Released_Year}</td>
+          <td>{item.Genre}</td>
+          <td>{item.IMDB_Rating}</td>
+        </tr>
+      })}
+    </table>
+  </div>
+  )
+}
 export default App;
+
+// "Poster_Link": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX67_CR0,0,67,98_AL_.jpg",
+// "Series_Title": "The Shawshank Redemption",
+// "Released_Year": "1994",
+// "Certificate": "A",
+// "Runtime": "142 min",
+// "Genre": "Drama",
+// "IMDB_Rating": 9.3,
+// "Overview": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+// "Meta_score": 80,
+// "Director": "Frank Darabont",
+// "Star1": "Tim Robbins",
+// "Star2": "Morgan Freeman",
+// "Star3": "Bob Gunton",
+// "Star4": "William Sadler",
+// "No_of_Votes": 2343110,
+// "Gross": "28,341,469",
+// "ID": "1"
