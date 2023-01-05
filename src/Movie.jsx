aa7bf9dd142501230348165be02972f504809f64
AliyaHuseynova <113546368+AliyaHuseynova@users.noreply.github.com>
2023-01-05 19:49:23 +0400

    .

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -16,4 +16,5 @@ export default function Movie() {
 return <div className="Movie">Movie</div>
 
 
+
 }   
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,12 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import './Navbar.css';
+// import '../Home.css';
 
 export default function Navbar(){
     return <div className="NavBar">
         <NavLink to="/" >Home</NavLink>
         <NavLink to="/moviepage" >Movie Page</NavLink>
-        <NavLink to="/contact" >Contact Us</NavLink>
-                    
+        <NavLink to="/contact" >Contact Us</NavLink>          
     </div>
 }
+
