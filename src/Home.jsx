e1b520764290bfb806fa54ebd752eca3fddebb8d
AliyaHuseynova <113546368+AliyaHuseynova@users.noreply.github.com>
2023-01-05 17:43:55 +0400

    test

---
+++ src/Home.jsx
@@ -0,0 +1,24 @@
+import
+
+export default function Home(){
+    return(
+        <div>
+            <div className="body">
+                <div className="hero"> </div>
+                    <div class="topnav">
+                    <a href="home">Home</a>
+                    <NavLink></NavLink>
+                </div>
+
+                <div className="banner">
+                    <div className="left-column">
+                        <h1>Projects</h1>
+                        <ol>
+                            <li>Personal Web Page & Digital Card - the goal of this assignment was to perform how much we have mastered HTML & CSS. There was created the web page, oriented on our personal preferences (our personal and contact, hobbies and etc). Additionally, we have provided the site with Digital Card</li><br/>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,3 +1,19 @@
-export function Movie() {
-    
-}
+import { useState } from "react";
+
+export default function Movie() {
+    const [movie,  setMovie] = useState([]);
+
+
+    fetch('https://reqbin.com/echo/get/json', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)))
+
+return <div className="Movie">Hello World</div>
+
+
+}   
