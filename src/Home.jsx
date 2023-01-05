1bfeb119f206802e1e8b60ce214a415684f56e09
AliyaHuseynova <113546368+AliyaHuseynova@users.noreply.github.com>
2023-01-05 18:42:48 +0400

    home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,15 @@ export default function Home(){
                         <h1>Projects</h1>
                         <ol>
                             <li>Personal Web Page & Digital Card - the goal of this assignment was to perform how much we have mastered HTML & CSS. There was created the web page, oriented on our personal preferences (our personal and contact, hobbies and etc). Additionally, we have provided the site with Digital Card</li><br/>
+                            <li>Weather App - in this assignment we have performed our JavaScript skills with some additional features, like usage of API and so on.</li> <br/>
+                            <li>Movie Rating App - our last assignment, which is mainly React-oriented.</li> <br/>
                         </ol>
+                        <h3>To visit our personal Github web sites, click buttons below:</h3>
+                        <form>
+                            <button class="button" formaction="https://mirvaricomps.github.io/website.github.io/"></button>
+                            <button class="button" formaction="https://aliyahuseynova.github.io/"></button>
+                            <button class="button" formaction="https://aghabayim.github.io"></button>
+                        </form>
                     </div>
                 </div>
             </div>
